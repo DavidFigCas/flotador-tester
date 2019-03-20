92ef7ee4ed42ae0181aead833d09b6904e34ff4a
David Figueroa <david.fc@inventoteca.com>
2019-03-19 18:07:14 -0600

    Ya tiene un argumento de posicion, adc y ohms

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #define   paso    2
 #define   dir     3
 #define   fin     4
+#define   pinADC  A0
 #define   pres    1
 #define   arriba  0
 #define   abajo   1
@@ -11,22 +12,54 @@
 bool sen = abajo;
 char letra;
 String argumento = "00000";
+int adc;
+int ohm;
+int pos;
+
+//----------------------------------------------------------- leer
+void leer()
+{
+  adc = analogRead(pinADC);
+  ohm = map(adc,0,1023,300,0);
+  Serial.print("ADC:\t");
+  Serial.print(adc);
+  Serial.print("\t");
+  Serial.print("OHM:\t");
+  Serial.print(ohm);
+  Serial.print("\t");
+  Serial.print("POS:\t");
+  Serial.println(pos);
+
+}
 
 //------------------------------------------------------------mover
 void mover(int dist)
 {
-  if(dist > 0)
-    digitalWrite(dir,arriba);
-  else
-    digitalWrite(dir,abajo);
+  int aux;
 
-  for(int i = 0;i<=dist;i++)
+
+  if(dist > 0)
+    sen = arriba;
+  else
+    sen = abajo;
+
+  digitalWrite(dir,sen);
+
+  for(int i = 0;i<abs(dist);i++)
   {
     digitalWrite(paso,1);
     delay(1);
     digitalWrite(paso,0);
     delay(1);
-    Serial.print(".");
+    if(sen == arriba)
+    {
+      pos++;
+    }
+    else
+    {
+      pos--;
+    }
+    //Serial.print(".");
   }
 
 }
@@ -34,14 +67,20 @@ void mover(int dist)
 //------------------------------------------------------------inicializar
 void inicializar()
 {
+  sen = abajo;
+  digitalWrite(dir,sen);
+  Serial.println("Buscando home...");
   while(digitalRead(fin) != pres)
   {
-    Serial.println(digitalRead(fin));
+    //Serial.println(digitalRead(fin));
     digitalWrite(paso,1);
     delay(1);
     digitalWrite(paso,0);
     delay(1);
+    //Serial.println("+");
   }
+  Serial.println("Home");
+  pos = -100;
 }
 
 //---------------------------------------------------------------------
@@ -57,15 +96,32 @@ void setup()
 
   digitalWrite(dir,sen);
   inicializar();
+  leer();
 }
 
 void loop()
 {
+  //leer();
+  delay(1000);
   if(Serial.available())
   {
-    //letra = Serial.read();
-    argumento = Serial.readStringUntil("OK");
-    mover(argumento.toInt());
+    letra = Serial.read();
+
+    switch(letra)
+    {
+      case '#':
+        argumento = Serial.readStringUntil("OK");
+        mover(argumento.toInt());
+        leer();
+      break;
+
+      case '$':
+        inicializar();
+        leer();
+      break;
+
+    }
+
   }
 
 }
