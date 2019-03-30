ac635b1fe3d2f1bd697b1f8d3ef35a596ef33e05
David Figueroa <david.fc@inventoteca.com>
2019-03-30 10:20:34 -0600

    Se realizaron las cuentas para el acme 8mm

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/*
+El mecanismo lleva un ACME 8mm, un motor de 200 pasos por vuelta y un driver de 16 micropasos
+Esto nos da una cuenta de 0.0025 mm por paso
+
+Utiliza un relevador conectado en serie a la alimentación de 5V que cierra un circuito
+si la bobina del motor a probar tiene continuidad, el circuito enciende el LED de un optoacoplador
+4N25, que está configurado en Emisor común y una resistencia de PullUp para indicar
+cuando hay o no continuidad, pasando la prueba el releavor se desactiva
+
+
+*/
+
 #include <Arduino.h>
 
 #define   paso    2
@@ -6,37 +18,42 @@
 #define   bomba   5
 #define   rele    6
 #define   pinADC  A0
-#define   pres    1
+#define   pres    0
 #define   arriba  0
 #define   abajo   1
-#define   lim_sup   2900
-#define   lim_inf   -99
+#define   lim_sup   30000
+#define   lim_inf   -10000
 #define   abierto   1
 #define   cerrado   0
-//1000 pasos = 6.98mm => 0.00698mm por pasos
+#define   pas_mm    0.0025
 
 bool sen = abajo;
 char letra;
 String argumento = "00000";
 int adc;
 int ohm;
-int pos;
+float pos;
+int pas;
 int aux = 0;
 int con = 0;
+int pulso = 100;
 
 
 //----------------------------------------------------------- continuidad
 void continuidad ()
 {
+//Se activa el relevador
   digitalWrite(rele,LOW);
   delay(100);
   con = digitalRead(bomba);
   digitalWrite(rele,HIGH);
-  if(con == cerrado)
+  //Se desactiva el releavor
+
+  if(con == cerrado) // Hay continuidad
   {
     Serial.println("Continuidad: OK");
   }
-  else
+  else              //no hay continuidad
   {
     Serial.println("Continuidad: NO OK!!!");
   }
@@ -46,79 +63,88 @@ void continuidad ()
 //----------------------------------------------------------- leer
 void leer()
 {
-  adc = analogRead(pinADC);
+  adc = analogRead(pinADC);             // Lee el canal analógico para determinar la resisitencia del flotador
   ohm = map(adc,0,1023,300,0);
 
-  Serial.print("ADC:\t");
+  Serial.print("ADC: ");
   Serial.print(adc);
   Serial.print("\t");
-  Serial.print("OHM:\t");
+  Serial.print("OHM: ");
   Serial.print(ohm);
   Serial.print("\t");
-  Serial.print("POS:\t");
+  Serial.print("POS: ");
   Serial.println(pos);
 
 }
 
 //------------------------------------------------------------mover
-void mover(int dist)
+void mover(float dist)                  // Dist es el argumento de desplazamiento en incremental
 {
-  aux = 0;
-  if(dist > 0)
-    sen = arriba;
-  else
-    sen = abajo;
+  aux = 0;                              // Esta variable se usa para evitar que se salga de los limites de movimiento
+                                        // Seleciono y escribo la direccion al motor
+  if(dist > 0)                          // Si la distancia es positiva
+    sen = arriba;                       // apunto el motor hacia arriba
+  else                                  // Si la distancia es negativa
+    sen = abajo;                        // apunto el motor hacia abajo
 
-  digitalWrite(dir,sen);
+  digitalWrite(dir,sen);                // Escribo el pin de direccion con el sentido
 
+                                        // Envio los pulsos para conseguir la dist
   for(int i = 0;(i<abs(dist) && (aux == 0)) ;i++)
   {
-    if(sen == arriba)
+    if(sen == arriba)                   // Si el sentido apunta arriba
     {
-      pos++;
-      if(pos > lim_sup)
+      pos = pos + pas_mm;               // Se incrementa la posicion
+
+      if(pos > lim_sup)                 // Si se supera el limite superior, cambia aux
       {
-        aux = 1;
+        aux = 1;                        // Aux indica que se debe salir del ciclo "for"
       }
     }
-    else
+    else                                // Si el sentido apunta hacia abajo
     {
-      pos--;
-      if((pos < lim_inf) || (digitalRead(fin) != pres))
+      pos = pos - pas_mm;
+      if((pos < lim_inf) || (digitalRead(fin) == pres)) // Si supero el limite inferior o detecto el fin
       {
-        aux = 1;
+        aux = 1;                        // Aux indica que debo salir del ciclo "for"
       }
     }
-    digitalWrite(paso,1);
-    delay(1);
-    digitalWrite(paso,0);
-    delay(1);
-    //Serial.print(aux);
+
+
+    if(aux == 0)                        // Si no estoy en algun limite, puedo dar pasos
+    {
+      digitalWrite(paso,1);             // Envio el pulso
+      delayMicroseconds(pulso);         // El retardo está controlado por un tiempo en uS
+      digitalWrite(paso,0);             // Termino el pulso
+      delayMicroseconds(pulso);
+      //Serial.print(aux);
+    }
+    //leer();
   }
 
 }
 
 //------------------------------------------------------------inicializar
-void inicializar()
+void inicializar()                      // Inicializa la posición en Home
 {
-  sen = abajo;
-  digitalWrite(dir,sen);
-  Serial.println("Buscando home...");
-  while(digitalRead(fin) != pres)
-  {
+  sen = abajo;                          // Apunto el sentido hacia abajo que es donde está el sensor
+  digitalWrite(dir,sen);                // Escribo la dirección en sentido abajo
+  Serial.println("Buscando home...");   // Envío la indicación de busqueda
+
+  while(digitalRead(fin) != pres)       // Mientras el botón de fin no esté presionado
+  {                                     // Debería existir un tiempo límite para terminar la busqueda
     //Serial.println(digitalRead(fin));
-    digitalWrite(paso,1);
-    delay(1);
+    digitalWrite(paso,1);               // Envío el pulsos
+    delayMicroseconds(pulso);
     digitalWrite(paso,0);
-    delay(1);
-    //Serial.println("+");
+    delayMicroseconds(pulso);           // Termino el pulso
+    //Serial.println("+");              // Repito los pulsos-pasos hasta tocar home
   }
-  Serial.println("Home");
-  pos = -100;
+  Serial.println("Home");               // Home encontrada
+  pos = lim_inf;                        // La posicion toma el limite inferior
 }
 
-//---------------------------------------------------------------------
-
+//--------------------------------------------------------------------- Setup
 void setup()
 {
   Serial.begin(9600);
@@ -137,6 +163,8 @@ void setup()
   //leer();
 }
 
+
+//------------------------------------------------------------- loop
 void loop()
 {
   //leer();
@@ -149,7 +177,7 @@ void loop()
     {
       case '#':
         argumento = Serial.readStringUntil("OK");
-        mover(argumento.toInt());
+        mover(argumento.toFloat());
         leer();
       break;
 
